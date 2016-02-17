be90d1b0f879a8375c33ce4382633ec5d8f22e96
Paul Sterl <puele@hotmail.com>
2016-02-17 12:07:19 +0100

    fixed 24dp generator added 36dp generator

--- src/main/java/org/sterl/svg2png/CliOptions.java
+++ src/main/java/org/sterl/svg2png/CliOptions.java
@@ -24,7 +24,8 @@ public enum CliOptions {
     ANDROID_LAUNCH(null, "android-launch", false, "Android Launcher Icon config mdpi 48x48 -> xxxhdpi 192x192."),
     ANDROID_ICON(null, "android-icon", false, "Android Icon (Action Bar, Dialog etc.)  config mdpi 36x36 -> xxxhdpi 128x128."),
     ANDROID_SMALL(null, "android-small", false, "Android Small default config from mdpi 24x24 -> xxxhdpi 96x96."),
-    ANDROID_24dp(null, "android-24dp", false, "Android 24dp icons, with suffix _24dp -- mdpi 24x24 -> xxxhdpi 96x96.")
+    ANDROID_24dp(null, "android-24dp", false, "Android 24dp icons, with suffix _24dp -- mdpi 24x24 -> xxxhdpi 96x96."),
+    ANDROID_36dp(null, "android-36dp", false, "Android 24dp icons, with suffix _36dp -- mdpi 36x36 -> xxxhdpi 144x011.")
     ;
     
     private final String shortName;
@@ -80,7 +81,13 @@ public enum CliOptions {
             }
         } else if (cmd.hasOption(ANDROID_24dp.longName)) {
             try {
-                result = m.readerFor(OutputConfig.class).readValue(CliOptions.class.getResourceAsStream("/android-launcher.json"));
+                result = m.readerFor(OutputConfig.class).readValue(CliOptions.class.getResourceAsStream("/android-24dp.json"));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else if (cmd.hasOption(ANDROID_36dp.longName)) {
+            try {
+                result = m.readerFor(OutputConfig.class).readValue(CliOptions.class.getResourceAsStream("/android-36dp.json"));
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
