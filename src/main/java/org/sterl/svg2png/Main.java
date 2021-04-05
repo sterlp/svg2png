30d9ed9d3491cb1488796096f1d1aa273ba0df0d
Paul Sterl <paul.sterl@outlook.de>
2021-04-05 12:37:01 +0200

    cleanup PR for XXE fix

--- src/main/java/org/sterl/svg2png/Main.java
+++ src/main/java/org/sterl/svg2png/Main.java
@@ -69,9 +69,9 @@ public class Main {
 
             return new Svg2Png(cfg).convert();
         } catch (TranscoderException e) {
-            Exception ex = e.getException();
+            final Exception ex = e.getException();
             if (ex.getMessage().contains("do not allow any external resources")) {
-                System.out.println("SVG tried to load resources, if this is okay use --unsecure option.");
+                System.out.println("SVG tried to load resources by URI, if this is okay use --unsecure option.");
             }
             throw new Svg2PngException(ex, cfg);
         } catch (Exception e) {
