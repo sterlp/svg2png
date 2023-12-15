b892d40b014f2dbaef245101c33f77db6499b6c1
Paul Sterl <paul@sterl.org>
2023-12-15 11:36:49 +0100

    extended tests

--- src/test/java/org/sterl/svg2png/config/TestFileConfigParsing.java
+++ src/test/java/org/sterl/svg2png/config/TestFileConfigParsing.java
@@ -1,68 +1,11 @@
 package org.sterl.svg2png.config;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.InputStream;
 
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class TestFileConfigParsing {
 
-    private final ObjectMapper m = new ObjectMapper();
-
-    @Test
-    public void testAndroid() throws Exception {
-        System.err.println(getClass().getResourceAsStream("./android.json"));
-        try (InputStream is = getClass().getResourceAsStream("/android.json")) {
-            OutputConfig config = m.readerFor(OutputConfig.class).readValue(is);
-            assertEquals(5, config.getFiles().size());
-            assertEquals(192, config.getFiles().get(0).getHeight());
-            assertEquals(192, config.getFiles().get(0).getWidth());
-            assertEquals("drawable-xxxhdpi", config.getFiles().get(0).getDirectory());
-        }
-    }
-    
-    @Test
-    public void testAndroidSmall() throws Exception {
-        try (InputStream is = getClass().getResourceAsStream("/android-small.json")) {
-            OutputConfig config = m.readerFor(OutputConfig.class).readValue(is);
-            assertEquals(5, config.getFiles().size());
-            assertEquals(96, config.getFiles().get(0).getHeight());
-            assertEquals(96, config.getFiles().get(0).getWidth());
-            assertEquals("drawable-xxxhdpi", config.getFiles().get(0).getDirectory());
-        }
-    }
-    
-    @Test
-    public void testAndroidIcon() throws Exception {
-        try (InputStream is = getClass().getResourceAsStream("/android-icon.json")) {
-            OutputConfig config = m.readerFor(OutputConfig.class).readValue(is);
-
-            assertEquals(5, config.getFiles().size());
-            assertEquals(192, config.getFiles().get(0).getHeight());
-            assertEquals(192, config.getFiles().get(0).getWidth());
-            assertEquals("drawable-xxxhdpi", config.getFiles().get(0).getDirectory());
-        }
-    }
-
-    @Test
-    public void testIos() throws Exception {
-        try (InputStream is = getClass().getResourceAsStream("/ios.json")) {
-            OutputConfig config = m.readerFor(OutputConfig.class).readValue(is);
-
-            assertEquals(15, config.getFiles().size());
-            assertEquals(20, config.getFiles().get(0).getHeight());
-            assertEquals(20, config.getFiles().get(0).getWidth());
-            assertEquals("ffffff", config.getNoAlpha());
-            assertTrue(config.isContentsJson());
-            assertNull(config.getFiles().get(0).getDirectory());
-        }
-    }
-    
     @Test
     public void testNameGeneration() {
         assertEquals("foo.png", FileOutput.buildName("foo", null, null, null));
